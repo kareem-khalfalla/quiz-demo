2e0a0f0682be55118d7537faa5a483dca2b14689
Kareem Khalfalla <kareem.khalfalla.dev@gmail.com>
2021-09-21 12:40:12 +0200

    can upload a file , can insert into db

--- adminpanel.php
+++ adminpanel.php
@@ -1,74 +1,77 @@
 <?php
-//session_start();
-//include('uploadquestfile.php');
-
 
 include 'config.php';
-	
 
- $uploaddir = 'C:\wamp64\www\efmphp';
-    $uploadfile = $uploaddir . basename($_FILES['fileupload']['tmp_name']);
+if (isset($_POST["submit"])) {
 
-	if(isset($_POST["submit"])) {
- 
+	$filePath = STORAGE_PATH . DIRECTORY_SEPARATOR . $_FILES['fileupload']['name'];
 
-    
-
-    if (move_uploaded_file($_FILES['fileupload']['tmp_name'], $uploadfile)) {
-        echo "File is valid, and was successfully uploaded.\n";
-    } else {
-        echo "Upload failed";
-    }
+	if (
+		move_uploaded_file(
+			$_FILES['fileupload']['tmp_name'],
+			$filePath
+		)
+	) {
+		echo '<p style="color: #080;">File is valid, and was successfully uploaded.</p>';
+	} else {
+		echo "Upload failed";
+	}
 }
 
-	
-	
-	
-	
-	 $myfile = fopen($uploadfile,"r") or die("Unable to open file!");
-	var_dump($uploadfile);
-  
-	while(!feof( $myfile)){
-		
-	$str = fgets($myfile);
-	$data=explode('|',$str);
-	
-	$question=$data[0];
-	$answerstring=$data[1];
-	$trueanswer=$data[2];
+$myFile = fopen($filePath, "r") or die("Unable to open file!");
 
-	$choices=explode(',',$answerstring);
-	
-	$choix1=$choices[0];
-	$choix2=$choices[1];
-	$choix3=$choices[2];
-		
-	
-	$sql="INSERT INTO `quiz`(`id`, `question`, `c1`, `c2`, `c3`, `answer`) VALUES (null,$question,$choix1,$choix2,$choix3,$trueanswer)";
-	mysqli_query($con,$sql);
-	
+$arr = [];
+while (!feof($myFile)) {
+	$str = fgets($myFile);
+	$data = explode('|', $str);
+
+	$question = $data[0];
+
+	if (count($data) == 1) {
+		break;
 	}
 
-  fclose($myfile);
-	
+	$answersString = $data[1];
+	$trueAnswer = $data[2];
+	$choices = explode(',', $answersString);
 
+	$choix1 = $choices[0];
+	$choix2 = $choices[1];
+	$choix3 = $choices[2];
+
+	$trueAnswer = str_replace('\'', '"', $trueAnswer);
+	$question = str_replace('\'', '"', $question);
+	$choix1 = str_replace('\'', '"', $choix1);
+	$choix2 = str_replace('\'', '"', $choix2);
+	$choix3 = str_replace('\'', '"', $choix3);
+
+	$sql = "INSERT INTO `quiz` SET 
+		`question` = '$question',
+		`c1` = '$trueAnswer',
+		`c2` = '$choix1',
+		`c3` = '$choix2',
+		`answer` = '$choix3'
+	";
+	mysqli_query($con, $sql);
+}
+
+fclose($myFile);
 
 ?>
 <html>
-<head>
-<meta charset="utf-8">
 
+<head>
+	<meta charset="utf-8">
 </head>
 
 <body>
 	<form method="post" enctype="multipart/form-data">
-		<input type="file" name="fileupload" id="fileupload" >
-	<input type="submit" name="submit" value="add" >
-	<input type="button" name="viewquiz" value="view quiz">
-	<input type="button" name="timemanage" value="time management">
-	<input type="button" name="result" value="user result">
+		<input type="file" name="fileupload" id="fileupload">
+		<input type="submit" name="submit" value="add">
+		<input type="button" name="viewquiz" value="view quiz">
+		<input type="button" name="timemanage" value="time management">
+		<input type="button" name="result" value="user result">
 	</form>
-
-	
 </body>
+
 </html>
