f9c38826421f5f4269c99d672db25a68ac5a4ea9
Kareem Khalfalla <kareem.khalfalla.dev@gmail.com>
2021-09-22 04:55:00 +0200

    fix config

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
-<?php 
+<?php
 
-$server = "localhost";
+const STORAGE_PATH = __DIR__ . DIRECTORY_SEPARATOR . 'storage';
+
+$server = "mysql";
 $user = "root";
-$pass = "";
-$database = "registerdb";
+$pass = "secret";
+$database = "my_dbname";
 
 $con = mysqli_connect($server, $user, $pass, $database);
 
 if (!$con) {
-    die("erreur:".mysqli_connect_error());
+    die("erreur:" . mysqli_connect_error());
 }
